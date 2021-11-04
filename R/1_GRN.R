bf0c69ef0843e87660485c88c4b41b2631582a5f
Emily Su <esu3@jhmi.edu>
2021-11-04 10:09:32 -0400

    updated for automatic time window detection, added TradeSeq

--- R/1_GRN.R
+++ R/1_GRN.R
@@ -13,6 +13,9 @@
 #' @param path vector of group names to include
 #' @param group_column column name in sampTab annotating groups in the path
 #' @param pseudotime_column column name in sampTab annotating pseudotime or latent time
+#' @param method method to find dynamic genes. Defaults to "gam", if "tradeseq", must provide expRaw
+#' @param expRaw raw expression matrix, required if method="tradeseq"
+
 #'
 #' @return pvals and cell info
 #' 
@@ -22,7 +25,9 @@ findDynGenes<-function(expDat,
                         sampTab, 
                         path=NULL, 
                         group_column="dpt_groups", 
-                        pseudotime_column="pseudotime"){
+                        pseudotime_column="pseudotime",
+                        method = "gam",
+                        expRaw=NULL){
 
   sampTab$dpt_groups<-sampTab[,group_column]
   sampTab$pseudotime<-sampTab[,pseudotime_column]
@@ -40,18 +45,53 @@ findDynGenes<-function(expDat,
   sampTab = sampTab[ids,]
   expDat = expDat[,ids]
 
-  t1 = sampTab$pseudotime
-  names(t1) = as.vector(sampTab$cell_name)
+  if (method!="tradeseq"){
+	  t1 = sampTab$pseudotime
+	  names(t1) = as.vector(sampTab$cell_name)
 
-  t1C = t1[ids]
-  cat("starting gammma...\n")
-  gpChr <-gamFit(expDat[,names(t1C)], rownames(expDat), t1C)
-  cells = data.frame(cell_name = names(t1), pseudotime = t1, group = as.vector(sampTab$dpt_groups))
-  rownames(cells)= names(t1)
-  cells = cells[order(cells$pseudotime),]
+	  t1C = t1[ids]
+	  cat("starting gammma...\n")
+	  gpChr <-gamFit(expDat[,names(t1C)], rownames(expDat), t1C)
+	  cells = data.frame(cell_name = names(t1), pseudotime = t1, group = as.vector(sampTab$dpt_groups))
+	  rownames(cells)= names(t1)
+	  cells = cells[order(cells$pseudotime),]
 
-  ans <- list( genes = gpChr, cells = cells)
+	  ans <- list( genes = gpChr, cells = cells)
+	}else{
+		if (is.null(expRaw)){
+			stop("Must provide expRaw for TradeSeq.")
+		}
+
+		require(tradeSeq)
+
+		# subset raw data based on normalized data
+		expRaw<-expRaw[,rownames(sampTab)]
+		expRaw<-expRaw[rownames(expDat),]
+
+		pt<-as.data.frame(sampTab[,pseudotime_column])
+		rownames(pt)<-rownames(sampTab)
+		colnames(pt)<-"pseudotime"
+		cw<-as.matrix(rep(1,nrow(pt)))
+		rownames(cw)<-rownames(sampTab)
+
+		ts<-tradeSeq::fitGAM(as.matrix(expRaw),pseudotime=as.matrix(pt),cellWeights=cw)
+		ATres<-associationTest(ts)
+
+		genes<-ATres$pvalue
+		names(genes)<-rownames(ATres)
+
+		cells<-data.frame(cell_name = sampTab$cell_name, pseudotime = sampTab$pseudotime, group = as.vector(sampTab$dpt_groups))
+		rownames(cells)<-sampTab$cell_name
+		cells<-cells[order(cells$pseudotime),]
+
+		ans<-list(genes=genes, cells=cells)
+
+	}
+  
+
   ans
+
+
 }
 
 
